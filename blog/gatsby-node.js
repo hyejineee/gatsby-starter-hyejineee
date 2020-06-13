f89955fd86adcac5ed0e955ddad45ea13bc560ab
hyeonhyejin <dev.hyejin04@gmail.com>
2020-06-13 20:18:05 +0900

    Setting gatsby-node

--- blog/gatsby-node.js
+++ blog/gatsby-node.js
@@ -1,7 +1,37 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
-
-// You can delete this file if you're not using it
+// query for events and create pages
+exports.createPages = async ({ actions, graphql, reporter }) => {
+    const basePath = "/"
+    
+    actions.createPage({
+      path: basePath,
+      component: require.resolve("./src/templates/post-list-template.js"),
+    })
+  
+    const result = await graphql(`
+      query {
+        allEvent(sort: { fields: startDate, order: ASC }) {
+          nodes {
+            id
+            slug
+          }
+        }
+      }
+    `)
+  
+    if (result.errors) {
+      reporter.panic("error loading events", result.errors)
+      return
+    }
+  
+    const events = result.data.allEvent.nodes
+    events.forEach(event => {
+      const slug = event.slug
+      actions.createPage({
+        path: slug,
+        component: require.resolve("./src/templates/post-template.js"),
+        context: {
+          eventID: event.id,
+        },
+      })
+    })
+  }
