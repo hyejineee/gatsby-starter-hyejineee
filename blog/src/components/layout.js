732aaafc64faaed9694ab5f637f0a7248f66de2b
hyeonhyejin <dev.hyejin04@gmail.com>
2020-07-15 13:49:41 +0900

    Clean up commit

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -1,28 +1,20 @@
-import React from "react";
+import React from "react"
 
-import style from "../styles/main.module.css";
-import Category from "./categories";
-import HeaderBio from "./header-bio";
-import useSiteMetadata from "../hooks/get-site-mata";
+import style from "../styles/main.module.css"
+import Header from "./header/header"
+import useSiteMetadata from "../hooks/get-site-mata"
 
 const Layout = ({ children }) => {
-  
-  const {siteMetadata} = useSiteMetadata();
+  const { siteMetadata } = useSiteMetadata()
 
   return (
     <div className={style.container}>
       <header className={style.header}>
-        <div>
-          <p> hyejineee.dev </p>
-        </div>
-        <div className={style.wrapper}>
-          <HeaderBio siteMetadata={siteMetadata} />
-          <Category categories={siteMetadata.categories} />
-        </div>
+        <Header />
       </header>
 
       <main className={style.wrapper}>{children}</main>
     </div>
-  );
-};
-export default Layout;
+  )
+}
+export default Layout
