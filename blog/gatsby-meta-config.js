d0586cc6deea7d7752495ed39905e6bbf2a677be
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-04 20:49:18 +0900

    Reset project

--- blog/gatsby-meta-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  title: `hyejineee`,
-  siteUrl: `http://hyejineee.github.io`,
-  description: `어떻게 살아야 하는가ㅏ🤔`,
-  imageUrl:
-    "https://img.velog.io/images/hyejineeee/profile/b45a3f40-bb4d-11e9-b819-59a898cb9ca6/-2019-08-10-6.03.13.png?w=240",
-  categories: ["Posts", "Daily Journals", "About me"]
-};
